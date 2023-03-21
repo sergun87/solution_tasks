e6e9d4852748e5bdfc685afd362b33b34eec66b4
Anton <antuan_dar@bk.ru>
2023-03-22 00:12:38 +0300

    initial commit

---
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/CountWordsSentence.java
@@ -0,0 +1,21 @@
+package ru.spb.taranenkoant.solution.tasks.strings;
+
+import java.util.List;
+
+/**
+ * Created by Anton on 22.03.2023
+ * description:
+ */
+public class CountWordsSentence {
+
+    public static void main(String args[]) {
+
+        String sentence = "Тут у нас рандомное предложение, любое ,      какое нравится ";
+
+        // не рабочий вариант, потому что выдает 9, а все таки слов отдельных у нас 8,
+        // а если пробелов много, то вообще может быть сколько угодно
+        String[] words = sentence.split(" ");
+        System.out.println(words.length);
+
+    }
+}
---
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/RandomString.java
@@ -0,0 +1,12 @@
+package ru.spb.taranenkoant.solution.tasks.strings;
+
+/**
+ * Created by Anton on 22.03.2023
+ * description:
+ */
+public class RandomString {
+
+    public static void main(String[] args) {
+
+    }
+}
